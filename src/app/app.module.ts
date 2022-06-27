c581fc9bb75713159bf207cc960d791f2fb7422a
decodedcreative <jhowell.1985@gmail.com>
2022-06-27 10:51:19 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderNavComponent } from './header/header-nav/header-nav.component';
-import { HeaderNavItemComponent } from './header/header-nav-item/header-nav-item.component';
-import { HeaderLinkComponent } from './header/header-link/header-link.component';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HeaderNavComponent,
-    HeaderNavItemComponent,
-    HeaderLinkComponent,
-  ],
+  imports: [BrowserModule, FormsModule, HeaderModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
